3f767a8188020c96df88b4dc6433c47050ba2e89
BrennanMcCulloch <bmccullo@ucsc.edu>
2019-11-07 15:56:22 -0800

    Toggle between Meshes!!
    we've got a system bois

---
+++ The Rift Prototype/Assets/Scripts/ToggleDead.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleDead : MonoBehaviour
+{
+
+	public GameObject deadStuff;
+	public GameObject deadMesh;
+	public GameObject aliveMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space")) 
+        {
+        	deadStuff.SetActive(!deadStuff.activeSelf);
+        	deadMesh.SetActive(!deadMesh.activeSelf);
+        	aliveMesh.SetActive(!aliveMesh.activeSelf);
+        }
+    }
+}
