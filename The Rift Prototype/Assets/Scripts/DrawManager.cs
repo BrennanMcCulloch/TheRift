86cb1cbfd7ab7bb6435a915f04d469727715f8d1
Brennan McCulloch <bmccullo@ucsc.edu>
2019-11-07 20:33:57 -0800

    Drawing on the screen.
    Don't mean anything yet, but it's there.

---
+++ The Rift Prototype/Assets/Scripts/DrawManager.cs
@@ -0,0 +1,47 @@
+﻿//Stole this online
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawManager : MonoBehaviour
+{
+    //When something is drawn, we make planes. Trail render. 
+    public GameObject drawPrefab;
+
+    private GameObject theTrail;
+    private Plane planeObj;
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planeObj = new Plane(Camera.main.transform.forward * -1, this.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetMouseButtonDown(0))
+        {
+            theTrail = (GameObject)Instantiate(drawPrefab, this.transform.position, Quaternion.identity);
+
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distance;
+
+            if(planeObj.Raycast(mouseRay, out distance))
+            {
+                startPos = mouseRay.GetPoint(distance);
+            }
+        }
+        else if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetMouseButton(0))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distance;
+
+            if (planeObj.Raycast(mouseRay, out distance))
+            {
+                theTrail.transform.position = mouseRay.GetPoint(distance);
+            }
+        }
+    }
+}
---
+++ The Rift Prototype/Assets/Scripts/FixDrawStart.cs
@@ -0,0 +1,16 @@
+﻿//Stole this online
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixDrawStart : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = Input.mousePosition;
+        temp.z = -10f;
+        this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+    }
+}
