9b80b7e38d8f97ad078645b5f5684243e9f6241c
Matt Milan <matt.milan@live.com>
2020-01-17 17:07:15 -0800

    WIP

---
+++ Assets/Scripts/Inventory/ItemPrerequisite.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueTree;
+
+namespace Inventory {
+
+    public class ItemPrerequisite : Prerequisite
+    {
+        public Item requisiteItem;
+
+        public override bool Met() {
+            return InventoryManager.Instance.itemBoxes.Contains(requisiteItem);
+        }
+    }
+}
