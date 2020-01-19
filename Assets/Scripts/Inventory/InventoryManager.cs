9b1e9b494778cb9431a4ae478008a72452bac4cc
Matt Milan <matt.milan@live.com>
2020-01-18 23:42:48 -0800

    Greatly alter the way we display items in the inventory UI and their addition/removal.

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -6,7 +6,7 @@ using System.Linq;
 namespace Inventory {
     public class InventoryManager : Singleton<InventoryManager>
     {
-        // A prefab button to hold an item in the ui
+        // Set these to the actual sizes of the UI elements
         const int NUM_ITEMS_HORIZONTAL = 4;
         const int ITEM_BOX_WIDTH_PIXELS = 200;
         const int ITEM_BOX_HEIGHT_PIXELS = 200;
@@ -14,51 +14,76 @@ namespace Inventory {
 
         public GameObject _itemBox;
         public GameObject itemPanel;
-
         public List<GameObject> itemBoxes;
 
-        void Awake() { BuildInventoryScreen(); }
-
-        public void AddItemToInventory(Item newItem){}
-
-        public void RemoveItemFromInventory(Item removeItem) {}
-
-        public void BuildInventoryScreen() {
-            for (int i = 0; i < 10; i++) {
-                itemBoxes.Add(BuildItemBox(null));
-                PlaceItemBox(itemBoxes.Last().gameObject);
+        // Moves the item from it's place in the world into the inventory UI
+        public void AddItem(Item newItem){
+            itemBoxes.Add(BuildItemBox(newItem));
+        }
+        public void AddItems(List<Item> items) {
+            foreach (Item item in items) {
+                Debug.Log("Adding item: " + item.name);
+                AddItem(item);
             }
-            // foreach (Item i in Inventory.items) {
-                //itemBoxes.Add(BuildItemBox(item));
-                //
-            //}
         }
 
-        public GameObject BuildItemBox(Item buildItem) {
-            GameObject newBox = Instantiate(_itemBox);
-            return newBox;
-            //Item i = buildItem.GetComponent<Item();
-            //buildItem.transform.parent = newBox.transform;
-            //buildItem.transform.position = Vector3.zero;
-        }
-
-        public void PlaceItemBox(GameObject itemBox) {
+        // Prepare a spot for the item in the UI, and display the item.
+        private GameObject BuildItemBox(Item item) {
+            // Create an itembox and add it to the UI
+            // Todo (matt) - calculate the vertical or horizontal position based ont he current number of items
+            GameObject itemBox = Instantiate(_itemBox);
             itemBox.transform.parent = itemPanel.transform;
-            itemBox.transform.localScale = Vector3.one;
-            float xPosition = ((itemBoxes.Count - 1) % NUM_ITEMS_HORIZONTAL) * ITEM_BOX_WIDTH_PIXELS + ITEM_BOX_PADDING_PIXELS;
-            float yPosition = ((itemBoxes.Count - 1) / NUM_ITEMS_HORIZONTAL) * ITEM_BOX_HEIGHT_PIXELS + ITEM_BOX_PADDING_PIXELS;
-            xPosition -= itemPanel.GetComponent<RectTransform>().rect.width * 2;
-            Debug.Log(xPosition);
-            Debug.Log(itemPanel.GetComponent<RectTransform>().rect.width / 2);
-            yPosition -= itemPanel.GetComponent<RectTransform>().rect.height / 2;
+            itemBox.transform.localPosition = Vector2.zero;
+            itemBox.transform.localScale = Vector2.one;
 
-            Vector2 itemBoxPosition = new Vector2(xPosition,-yPosition);
-            itemBox.transform.localPosition = itemBoxPosition;
+
+            itemBox.transform.localPosition = NextItemBoxPosition();
+
+            // Move the item to the itembox
+            Transform t = item.transform;
+            t.parent = itemBox.transform;
+            t.localPosition = Vector2.zero;
+            t.localScale = Vector2.one;
+
+            // Now we want to strip out all the dialogue stuff. This item is now just an item with a description.
+            foreach (Transform child in item.transform) {
+                GameObject.Destroy(child.gameObject);
+            }
+            Destroy(item.GetComponent<DialogueTrigger>());
+
+            return itemBox;
         }
 
-        public void ToggleInventoryWindow() {
-            gameObject.SetActive(!gameObject.active);
+        // Destroys the item's parent. This gets rid of the UI element as well as the item.
+        public void RemoveItem(Item removeItem) {
+            GameObject.Destroy(removeItem.transform.parent.gameObject);
+            CleanUpPanel();
         }
 
+        public bool HasItem(Item hasItem) {
+            return Items().Contains(hasItem);
+        }
+
+        public List<Item> Items() {
+            return itemPanel.GetComponentsInChildren<Item>().ToList<Item>();
+        }
+
+        // Called whenever we need to reorganize the inventory panel, specifically after removing an item.
+        private void CleanUpPanel() {
+            // Orphan items from their container so they dont get destroyed with the containers
+            foreach (Item item in Items()) item.transform.SetParent(null);
+
+            // Destroy the containers
+            foreach (GameObject itemBox in itemBoxes) GameObject.Destroy(itemBox);
+
+            // Re-add the items, creating fresh containers
+            foreach (Item item in Items()) AddItem(item);
+        }
+
+        private Vector2 NextItemBoxPosition() {
+            float yPosition = (itemBoxes.Count * ITEM_BOX_HEIGHT_PIXELS) - (itemPanel.GetComponent<RectTransform>().rect.height / 4);
+            Debug.Log(yPosition);
+            return new Vector2(0, yPosition);
+        }
     }
 }
