7463947dd5fdcbecb88958216cc60a2f5f1e04ab
ajperrot <alexperrotti@gmail.com>
2020-01-19 23:39:44 -0800

    prefab LocationNarration for location based narration

---
+++ Assets/Scripts/SoundOnPlayerContactOnce.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnPlayerContactOnce : MonoBehaviour
+{
+    // Play the sudio source's sound when the player touches this, then disable the collider
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<AudioSource>().Play();
+            GetComponent<SphereCollider>().enabled = false;
+        }
+    }
+    
+}
