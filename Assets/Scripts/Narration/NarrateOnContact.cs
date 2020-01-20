ae484428f11fe18d2edf06401c862035f2bf7fe9
ajperrot <alexperrotti@gmail.com>
2020-01-20 01:45:45 -0800

    narration now handled by singleton on the player character, and minor reorganization

--- Assets/Scripts/Narration/NarrateOnContact.cs
+++ Assets/Scripts/Narration/NarrateOnContact.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundOnPlayerContactOnce : MonoBehaviour
+public class NarrateOnContact : MonoBehaviour
 {
+    public AudioClip clip;
+
     // Play the sudio source's sound when the player touches this, then disable the collider
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            GetComponent<AudioSource>().Play();
-            GetComponent<SphereCollider>().enabled = false;
+            Narration.narrate(clip);
         }
     }
     
