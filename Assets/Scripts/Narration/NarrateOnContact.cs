07b487c574b412e35d395a5c37e9748a312c1de2
ajperrot <alexperrotti@gmail.com>
2020-01-20 18:02:26 -0800

    NarrateOnContact disables itself after the first use

--- Assets/Scripts/Narration/NarrateOnContact.cs
+++ Assets/Scripts/Narration/NarrateOnContact.cs
@@ -6,12 +6,21 @@ public class NarrateOnContact : MonoBehaviour
 {
     public AudioClip narrationClip;
 
+    private bool narrationReady = false;
+
+    // Initialize narrationReady Properly
+    void Start ()
+    {
+        if (narrationClip != null) narrationReady = true;
+    }
+
     // Play the sudio source's sound when the player touches this, then disable the collider
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (narrationReady == true && other.gameObject.tag == "Player")
         {
             Narration.Narrate(narrationClip);
+            narrationReady = false;
         }
     }
     
