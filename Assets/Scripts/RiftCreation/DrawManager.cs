e5f6a34a7472a66926f3493543d01039d3369199
Matt Milan <matt.milan@live.com>
2020-01-21 17:44:01 -0800

    Add deadzone to drawmanager so that brief navigation clicks are not considered a draw action. General refactor.

--- Assets/Scripts/RiftCreation/DrawManager.cs
+++ Assets/Scripts/RiftCreation/DrawManager.cs
@@ -5,22 +5,24 @@ using UnityEngine;
 
 public class DrawManager : MonoBehaviour
 {
+    // minimum distance must be dragged before drawing kicks in. helps separate player nav and drawing actions
+    const float MINIMUM_DRAW_DISTANCE = 50.0f;
+    // todo (matt) - magic number from code. shuold ask brennan what this was for
+    const float SCREEN_TO_WORLD_ADJUSTMENT = 5.0f;
     //This makes the class act as a singleton
     public static DrawManager instance;
-
-    //When something is drawn, we make planes. Trail render. 
+    //When something is drawn, we make planes. Trail render.
     public GameObject drawPrefab;
-
-    //private LineRenderer line;
+    // Track where we started touching/dragging. Used to calculate deadzone
+    private Vector3 startedHoldingPosition;
     private GameObject theTrail;
     private Plane planeObj;
     private List<Vector3> points;
     private List<Segment> segments;
-    private Vector3 startPos;
     private float distance;
     private int loopStart = 0;
     private int loopEnd = 0;
-
+    private bool drawing;
 
     // Awake is called once before start
     void Awake()
@@ -33,63 +35,92 @@ public class DrawManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // call this here instead of awake to make sure the camera object is ready
         planeObj = new Plane(Camera.main.transform.forward * -1, this.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //handle first frame of input
-        if((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetMouseButtonDown(0))
-        {
-            //formerly FixDrawStart
-            Vector3 temp = Input.mousePosition;
-            temp.z = 5f;
-            this.transform.position = Camera.main.ScreenToWorldPoint(temp);
-            //replace/create trail
-            Destroy(theTrail);
-            points.Clear();
-            segments.Clear();
-            loopEnd = 0;
-            loopStart = 0;
-            theTrail = (GameObject)Instantiate(drawPrefab, this.transform.position, Quaternion.identity);
-            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(planeObj.Raycast(mouseRay, out distance))
-            {
-                Vector3 point = mouseRay.GetPoint(distance);
-                startPos = point;
-                addPoint(point);
-            }
-        }
-        //mouse held down
-        else if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetMouseButton(0))
-        {
-            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+             if (InputStarts)    SetDrawOrigin();
+        else if (InputContinues) DrawLine();
+        else if (InputEnds)      FinishDrawing();
+    }
 
-            if(planeObj.Raycast(mouseRay, out distance))
+    // Make a note of where we started drawing.
+    void SetDrawOrigin() {
+        startedHoldingPosition = Input.mousePosition;
+    }
+
+    // Use a deadzone for draw detection so we can separate drawing actions from navigation actions
+    void DrawLine() {
+        // Bail unless we're already drawing, or we're too close to the start position.
+        if (!drawing && !DraggedPastThreshold(startedHoldingPosition)) return;
+
+        if (drawing) {
+            DrawTrail();
+        }
+        else {
+            ResetTrail();
+            drawing = true;
+        }
+    }
+
+    // Tries to open a rift when we stop drawing
+    void FinishDrawing() {
+        //create mesh if there was a collision
+        if(loopEnd - loopStart > 5)
+        {
+            RiftMeshManager.Create(loopStart, loopEnd, points);
+        }
+        //destroy the game object 6 seconds after drawing
+        Destroy(theTrail, 6);
+        drawing = false;
+    }
+
+    // Executed every frame while the user is touching/dragging and defines the shape of our line via points
+    void DrawTrail() {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(planeObj.Raycast(mouseRay, out distance))
+        {
+            Vector3 point = mouseRay.GetPoint(distance);
+            if(!points.Contains(point))
             {
-                Vector3 point = mouseRay.GetPoint(distance);
-                if(!points.Contains(point))
-                {
-                    addPoint(point);
-                }
+                AddPoint(point);
             }
         }
-        //On mouse up. Bound to need to use this.
-        else if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetMouseButtonUp(0))
+    }
+
+    // When user begins drawing, clean and prepare trail data
+    void ResetTrail() {
+        ClearTrail();
+        StartTrail();
+    }
+
+    // Cleans up all the trail elements before drawing a new trail
+    void ClearTrail() {
+        Destroy(theTrail);
+        points.Clear();
+        segments.Clear();
+        loopEnd = 0;
+        loopStart = 0;
+    }
+
+    // Refreshes positioning data and starts a new trail at the current mousePosition-to-screenPoint.
+    void StartTrail() {
+        this.transform.position = Camera.main.ScreenToWorldPoint(startedHoldingPosition + new Vector3(0f,0f,SCREEN_TO_WORLD_ADJUSTMENT));
+        theTrail = (GameObject)Instantiate(drawPrefab, this.transform.position, Quaternion.identity);
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(planeObj.Raycast(mouseRay, out distance))
         {
-            //create mesh is there was a collision
-            if(loopEnd - loopStart > 5)
-            {
-                RiftMeshManager.Create(loopStart, loopEnd, points);
-            }
-            //destroy the game object 6 seconds after drawing
-            Destroy(theTrail, 6);
+            Vector3 point = mouseRay.GetPoint(distance);
+            AddPoint(point);
         }
     }
 
     // Adds a point and handles what to do if a rift was drawn
-    private void addPoint(Vector3 point) {
+    private void AddPoint(Vector3 point) {
         //add point
         points.Add(point);
         theTrail.transform.position = point;
@@ -101,7 +132,7 @@ public class DrawManager : MonoBehaviour
             {
                 if (Vector3.Distance(point, points[i]) < 0.15f)
                 {
-                    handleLoop(i, point);
+                    HandleLoop(i, point);
                     return;
                 }
             }
@@ -112,14 +143,14 @@ public class DrawManager : MonoBehaviour
                 Vector3 intersection = Segment.Intersection(segments[segments.Count - 1], segments[i]);
                 if(intersection != Vector3.zero && loopEnd == 0)
                 {
-                    handleLoop(i, intersection);
+                    HandleLoop(i, intersection);
                 }
             }
         }
     }
 
     //Sets variables to represent the created loop if it is big enough
-    private void handleLoop(int index, Vector3 intersection)
+    private void HandleLoop(int index, Vector3 intersection)
     {
         if(points.Count - index > 10)
         {
@@ -129,5 +160,25 @@ public class DrawManager : MonoBehaviour
             loopEnd = points.Count - 1;
             return;
         }
+    }
+
+    // When finger or mouse have just been pushed down
+    bool InputStarts {
+        get {return Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began); }
+    }
+
+    // When finger or mouse are already down and moving
+    bool InputContinues {
+        get { return Input.GetMouseButton(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved); }
+    }
+
+    // When finger or mousebutton is lifted
+    bool InputEnds {
+        get { return Input.GetMouseButtonUp(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended); }
+    }
+
+    // Determine whether a given Vector3 is far enough away from the starting Vector3
+    bool DraggedPastThreshold(Vector3 startingPosition) {
+        return Vector3.Distance(Input.mousePosition, startingPosition) > MINIMUM_DRAW_DISTANCE;
     }
 }
