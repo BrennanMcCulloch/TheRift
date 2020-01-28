7dc0947631ca60f40b3676caf8e4bb295d402558
ajperrot <alexperrotti@gmail.com>
2020-01-28 02:47:09 -0800

    WordScramble changes text into an anagram of itself every frame

---
+++ Assets/Scripts/WordScamble.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+
+public class WordScamble : MonoBehaviour
+{
+    private Text textComp;
+    private char[] baseWord;
+    private char[] newWord;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComp = GetComponent<Text>();
+        baseWord = textComp.text.ToCharArray();
+        newWord = new char[baseWord.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < newWord.Length; i++)
+        {
+            newWord[i] = baseWord[Random.Range(0, baseWord.Length)];
+        }
+        textComp.text = new string(newWord);
+    }
+}
