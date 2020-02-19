550f86543065b3bf98e52219aaeb9c375f5b67d4
ajperrot <alexperrotti@gmail.com>
2020-02-18 16:02:32 -0800

    simple script to apply material to a camera's rendering

---
+++ Assets/Scripts/ApplyFilter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyFilter : MonoBehaviour
+{
+    public Material effectMaterial;
+
+    // Apply a Material to src before render to dest
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        // Copy the source Render Texture to the destination,
+        // applying the material along the way.
+        Graphics.Blit(src, dest, effectMaterial);
+    }
+}
